6aee0e557114d27c6bdc838252b8e48d033cbe72
bogdanpescarus <bogdan.pescarus94@gmail.com>
2021-10-11 13:54:01 +0300

    modified the .cpp file

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,9 +1,60 @@
 #include "thread_pool.hpp"
 
-ThreadPool::ThreadPool() {
-
-
+ThreadPool::ThreadPool(int numThreads) {
+    start(numThreads);
 }
 
+ThreadPool::~ThreadPool() {
+    stop();
+}
 
+void ThreadPool::start(int numThreads) {
+    for(int i = 0; i < numThreads; ++i) {
+		mThreads.emplace_back([=] () ->  void {
+			while(true) {
+				Task _task;
+				{
+					/* this lock is not needed when executing the _task(), 
+					   but only while manipulating the queue and also when working
+					   with the condition variable.
+					   So, when _task() should be executed outside of the scope of 
+					   the unique lock. */
+					std::unique_lock<std::mutex> ul(mMtx);
 
+					mCv.wait(ul, [=] { return (mStop || !mTasks.empty()); });
+
+					/* We need the second condition so that the we wait for all 
+					   of the tasks to be finished (and removed from the queue) */
+					if(mStop && mTasks.empty()) {
+						break;
+					}
+
+					_task = mTasks.front();
+					mTasks.pop();
+				}
+				_task();
+			}
+		});
+	}
+}
+
+void ThreadPool::stop() noexcept {
+	std::unique_lock<std::mutex> ul(mMtx);
+	mStop = true;
+	ul.unlock();
+
+	mCv.notify_all();
+	/* it needs auto& (or std::thread&) and not just auto (or std::thread), because
+	   the copy constructor of thread class was deleted */
+	for(std::thread& thread : mThreads) {
+		thread.join();  
+    }  
+}
+
+void ThreadPool::enqueue(Task _task) {
+	std::unique_lock<std::mutex> ul(mMtx);
+	mTasks.push(_task);
+	ul.unlock();
+
+	mCv.notify_one();
+}
--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <thread>
 #include <functional>
+#include <condition_variable>
 #include <vector>
-
-#pragma once
+#include <queue>
 
 #define max_threads 8
 
 class ThreadPool {
 	private:
-		std::queue<std::function<void()> mTasks;
+		using Task = std::function<void()>;
 		std::vector<std::thread> mThreads;
+
+		/* this is for the actual tasks that need to use 
+		   the thread pool */
+		std::queue<Task> mTasks;
+
+		std::mutex mMtx;
+		std::condition_variable mCv;
+		bool mStop = false;
 		
 	public:
-		ThreadPool();
+		
 
+		ThreadPool(int);
+		
+		void start(int);
+		void stop() noexcept;
+
+		void enqueue(Task);
+
+		~ThreadPool();
 };
